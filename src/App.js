978b149ae367f4845b36ad4d3423fc23f784cc16
Brandon Whisnant <whisnantb@locustec.com>
2018-09-24 16:05:58 -0400

    added more styles and a little functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
+import Contact from './components/Contact'; 
+import Header from './components/Header'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
+    // we can put js expressions here
+    // const name = 'Larry';
+    // const showHello = false;
+    // const showMath = true;
+    // const num1 = 40;
+    // const num2 = 23;
+
+    // let math;
+    // if (showMath) {
+    //   math = <h4>{num1} + {num2} = {num1 + num2}</h4>; 
+    // } else {
+    //   math = null;
+    // }
     return (
       <div className="App">
-        <h1>The App Component</h1>
+        <Header branding="Contact Manager"/>
+        <div className="container">
+          <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
+          <Contact name="Karen Smith" email="karen@gmail.com" phone="333-333-3333" />
+        </div>
+        
       </div>
     );
   }
